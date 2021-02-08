d17cb8439098c3efefae7e6c8b78829d52fef91b
xkand1 <55233346+xkand1@users.noreply.github.com>
2021-02-08 17:54:07 +0900

    Add files via upload

--- main1.py
+++ main1.py
@@ -1,9 +1,11 @@
 def Summirovanie(x, y):
    return x + y
+def Vychitanie(x, y):
+   return x - y
 
 print("Vyberite zadachu.")
 print("1.Summirovanie")
-
+print("2.Vychitanie")
 
 choice = input("Выберите одно(1/2/3/4):")
 num1 = int(input("Введите первое число: "))
@@ -12,3 +14,5 @@ num2 = int(input("ВВедите второе число: "))
 if choice == '1':
    print(num1,"+",num2,"=", Summirovanie(num1,num2))
 
+elif choice == '2':
+   print(num1,"-",num2,"=", Vychitanie(num1,num2))
