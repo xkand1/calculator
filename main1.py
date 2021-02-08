4c916b87dd19c1e8041fd038c33232643ff4ae66
xkand1 <55233346+xkand1@users.noreply.github.com>
2021-02-08 17:56:35 +0900

    calculator +-*

--- main1.py
+++ main1.py
@@ -2,10 +2,13 @@ def Summirovanie(x, y):
    return x + y
 def Vychitanie(x, y):
    return x - y
+def Umnojenie(x, y):
+   return x * y
 
 print("Vyberite zadachu.")
 print("1.Summirovanie")
 print("2.Vychitanie")
+print("3.Umnojenie")
 
 choice = input("Выберите одно(1/2/3/4):")
 num1 = int(input("Введите первое число: "))
@@ -16,3 +19,6 @@ if choice == '1':
 
 elif choice == '2':
    print(num1,"-",num2,"=", Vychitanie(num1,num2))
+
+elif choice == '3':
+   print(num1,"*",num2,"=", Umnojenie(num1,num2))
